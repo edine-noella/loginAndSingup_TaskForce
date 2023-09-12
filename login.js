d212abd13f3742878180bb2aa25908075aa55ba4
edine-noella <edinenoella@gmail.com>
2023-09-12 15:42:42 +0200

    Feature (added form validations)
    - added validations for singup and login pages

--- login.js
+++ login.js
@@ -1,17 +1,29 @@
 document.getElementById("login-form").addEventListener("submit", function (e) {
     e.preventDefault();
     // Get input values
-    const username = document.getElementById("login-username").value;
+    const email = document.getElementById("login-email").value;
     const password = document.getElementById("login-password").value;
     const rememberMe = document.getElementById("remember-me").checked;
 
-    // Perform validation and login logic here
-    // You can add your validation code and login logic here
-    // For example, you can send a request to your server to authenticate the user.
+    // Validate email
+    if (!email.includes("@")) {
+        alert("Please enter a valid email address.");
+        return;
+    }
 
-    // If "Remember Me" is checked, you can store the user's credentials securely (e.g., using cookies or localStorage).
+    // Validate password
+    if (password.length < 8) {
+        alert("Password must be at least 8 characters long.");
+        return;
+    }
 
-    // Clear input fields
+    const regex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{8,}$/;
+    if (!regex.test(password)) {
+        alert("Password must contain at least one lowercase letter, one uppercase letter, one number, and one special character.");
+        return;
+    }
+
+    // Clear input values
     document.getElementById("login-username").value = "";
     document.getElementById("login-password").value = "";
 });
